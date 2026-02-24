1f71055834d55cdf21d3bcd32a2f39b5b44bc242
Прозоров Владислав <159719052+vladislavprozorov@users.noreply.github.com>
2026-02-24 13:33:19 -0800

    chore(commitlint): add perf type to allowed list (#14)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "refactor", "chore", "docs", "test"]],
+    "type-enum": [2, "always", ["feat", "fix", "refactor", "chore", "docs", "test", "perf"]],
   },
 };
